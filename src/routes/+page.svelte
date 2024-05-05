1ca4fdb3e4c5235dc84ef43ece455b3da76a7bc4
Andreas Söderlund <ciscoheat@gmail.com>
2024-05-05 21:13:06 +0200

    Reproduced error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts"></script>
+
+<!-- Remove the whitespace before the colon to remove error -->
+{#snippet foo(test : string[])}
+	<button>{test.join(", ")}</button>
+{/snippet}
+{@render foo(["a", "b"])}
